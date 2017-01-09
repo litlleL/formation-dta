5016dadce150d14fcd6a0445b4a5261b66036acf
Livio Asdrubal <asdrubal.livio@gmail.com>
2017-01-09 09:55:16 +0100

    modif mineur

--- livio-pizzeria-project/livio-pizzeria-parent/livio-pizzeria-dao/src/main/java/fr/pizzeria/aspect/config/PizzeriaAppAspectAppConfig.java
+++ livio-pizzeria-project/livio-pizzeria-parent/livio-pizzeria-dao/src/main/java/fr/pizzeria/aspect/config/PizzeriaAppAspectAppConfig.java
@@ -1,12 +1,10 @@
 package fr.pizzeria.aspect.config;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
 @EnableAspectJAutoProxy
-@ComponentScan("fr.pizzeria.aspect.service")
 public class PizzeriaAppAspectAppConfig {
 
 }
